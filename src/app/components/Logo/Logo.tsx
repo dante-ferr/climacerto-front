bdcc72fe3d64ffba5d5f7ae05354eaf89eccc347
Dante <dante.tf5@gmail.com>
2025-10-04 19:41:20 -0300

    add logo

---
+++ src/app/components/Logo/Logo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Typography } from "@mui/joy";
+
+function Logo() {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", gap: 1.5, mb: 2 }}>
+      <img src="/logo.ico" alt="ClimaCerto logo" width="32" height="32" />
+      <Typography level="h3" component="h1">
+        ClimaCerto
+      </Typography>
+    </Box>
+  );
+}
+
+export default Logo;
--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -1,30 +1,37 @@
 "use client";
 
 import React from "react";
+import Box from "@mui/joy/Box";
+import Divider from "@mui/joy/Divider";
+import Sheet from "@mui/joy/Sheet";
+import Typography from "@mui/joy/Typography";
 import { useLocation } from "@/app/context/LocationContext";
 import SearchForm from "../SearchForm";
+import Logo from "../Logo";
 import styles from "./sidebar.module.scss";
-import Box from "@mui/joy/Box";
-import Typography from "@mui/joy/Typography";
-import Divider from "@mui/joy/Divider";
 
 function Sidebar() {
   const { selectedLocation } = useLocation();
 
   return (
-    <aside className={styles.sidebar}>
-      <SearchForm />
-      {selectedLocation && (
-        <Box sx={{ mt: 4 }}>
-          <Divider sx={{ mb: 2 }} />
-          <Typography level="h4" component="h2" sx={{ mb: 1 }}>
-            Localização Selecionada
-          </Typography>
-          <Typography>Latitude: {selectedLocation.lat.toFixed(4)}</Typography>
-          <Typography>Longitude: {selectedLocation.lng.toFixed(4)}</Typography>
-        </Box>
-      )}
-    </aside>
+    <Sheet component="aside" className={styles.sidebar}>
+      <Logo />
+      <Box>
+        <SearchForm />
+        {selectedLocation && (
+          <Box sx={{ mt: 4 }}>
+            <Divider sx={{ mb: 2 }} />
+            <Typography level="h4" component="h2" sx={{ mb: 1 }}>
+              Localização Selecionada
+            </Typography>
+            <Typography>Latitude: {selectedLocation.lat.toFixed(4)}</Typography>
+            <Typography>
+              Longitude: {selectedLocation.lng.toFixed(4)}
+            </Typography>
+          </Box>
+        )}
+      </Box>
+    </Sheet>
   );
 }
 
